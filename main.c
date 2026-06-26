35bf1e29d21d3d9635336e32831514318a70005c
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-06-26 01:45:36 -0400

    mostly functional lexer

--- main.c
+++ main.c
@@ -29,20 +29,33 @@ typedef struct {
 	TokenType type;
 } Token;
 
+char *stringFromToken(TokenType t){
+	char *strings[] = { "INVALID", "NULL_TYPE", "OBJ_BEGIN", "OBJ_END", "STRING", "CHAR", "PUNCTUATOR", "NUMBER", "BOOLEAN", "COMMA", "ARR_BEGIN", "ARR_END", "FILE_EOF" };
+
+	return strings[t];
+}
+
+
+
 typedef struct {
 	
 } Lexer;
 
 Token get_string_token(FILE* fp){
-	char buf[2];
-	char lex[2048];
+	char copy[2];
+	char lex[2048] = "";
 	Token token;
 	strcat(lex, "\"");
-	while(fgets(buf, sizeof buf, fp) != NULL){
-		char curr = buf[0];
-		strcat(lex, &curr);
+	int curr;
+	while((curr = fgetc(fp)) != EOF){
+		copy[0] = curr;
+		copy[1] = '\0';
+		strcat(lex, copy);
+		printf("Current string: %s\n", lex);
 		if(curr == '\"'){
-			token.lexeme = lex;
+			char *copy = malloc(strlen(lex) + 1);
+			strcpy(copy, lex);
+			token.lexeme = copy;
 			token.type = STRING;
 			return token;
 		}
@@ -56,14 +69,14 @@ Token get_string_token(FILE* fp){
 
 typedef enum {
 	START,
-	INTEGER,
-	DOUBLE,
-	SCIENTIFIC_NOTATION,
-	__VALID_END_STATES,
 	INTEGER_WITH_DOT,
 	INITIAL_MINUS_SIGN,
 	SCIENTIFIC_E,
 	SCIENTIFIC_E_WITH_SIGN,
+	__VALID_END_STATES,
+	INTEGER,
+	DOUBLE,
+	SCIENTIFIC_NOTATION,
 	__LAST_STATE
 } NUMERIC_FSM_STATES_t;
 
@@ -72,6 +85,12 @@ typedef struct {
 	char *tokens;
 	NUMERIC_FSM_STATES_t next_state;
 } NUMERIC_FSM_INFO_t;
+
+char *stringFromState(TokenType t){
+	char *strings[] = { "START", "INTEGER_WITH_DOT", "INITIAL_MINUS_SIGN", "SCIENTIFIC_E", "SCIENTIFIC_E_WITH_SIGN", "__VALID_END_STATES", "INTEGER", "DOUBLE", "SCIENTIFIC_NOTATION", "__LAST_STATE" };
+
+	return strings[t];
+}
 
 // check if the token is a number using FSMs
 Token get_numeric_token(FILE* fp, char first){
@@ -94,60 +113,91 @@ Token get_numeric_token(FILE* fp, char first){
 	};
 	NUMERIC_FSM_STATES_t current_state = START;
 	NUMERIC_FSM_INFO_t *p = state_machine;
-	NUMERIC_FSM_INFO_t *found;
+	NUMERIC_FSM_INFO_t *found = NULL;
 
-	char buf[2];
-	char lex[2048];
+	char lex[255] = "";
 	Token token;
-	int has_dot = 0;
+	char copy[2];
 	while(p->state != __LAST_STATE){
-		if (strchr(p->tokens, first) != NULL){
-			found = state_machine;
-			break;
+		if (p->state == current_state){
+			if (strchr(p->tokens, first) != NULL){
+				printf("Found transition from initial state\n");
+				found = state_machine;
+				break;
+			}
 		}
 		p++;
 	}
 	if(found != NULL){
+		printf("Transitioning to the next state\n");
 		current_state = p->next_state;
 	}else{
-		printf("Invalid character: %s", &first);
-		token.lexeme = &first;
+		printf("Invalid character: %c\n", first);
+		copy[0] = first;
+		copy[1] = '\0';
+		strcat(lex, copy);
+		char *copy2 = malloc(strlen(lex) + 1);
+		strcpy(copy2, lex);
+		token.lexeme = copy2;
 		token.type = INVALID;
 		return token;
 	}
-	strcat(lex, &first);
-	while(fgets(buf, sizeof buf, fp) != NULL){
-		char curr = buf[0];
-		// TODO: check for delimiter characters before state processing
-		// 
+	copy[0] = first;
+	copy[1] = '\0';
+	printf("First strcat: %s\n", copy);
+	strcat(lex, copy);
+	printf("After strcat: %s\n", lex);
+	int curr;
+	while((curr = fgetc(fp)) != EOF){
+		found = NULL;
+		printf("current char: %c\n", curr);
+		// check for delimiter characters before state processing
+		if(strchr(",}]", curr )){
+			ungetc(curr, fp);
+			char *copy2 = malloc(strlen(lex) + 1);
+			strcpy(copy2, lex);
+			token.lexeme = copy2;
+			break;
+		}
 		//
 		p = state_machine;
 		while(p->state != __LAST_STATE){
-			if (strchr(p->tokens, curr) != NULL){
-				found = state_machine;
-				break;
+			if (p->state == current_state){
+				if (strchr(p->tokens, (char)curr) != NULL){
+					printf("found matching transition with tokens %s to %s\n", p->tokens, stringFromState(p->next_state));
+					found = state_machine;
+					break;
+				}
 			}
 			p++;
 		}
-		strcat(lex, &curr);
 		if(found != NULL){
 			current_state = p->next_state;
+			copy[0] = curr;
+			copy[1] = '\0';
+			strcat(lex, copy);
 		}else{
-			printf("Invalid character: %s", &first);
-			token.lexeme = lex;
+			printf("Invalid character: %c\n", curr);
+			copy[0] = curr;
+			copy[1] = '\0';
+			strcat(lex, copy);
+			printf("Final invalid token: %s\n", lex);
+			char *copy2 = malloc(strlen(lex) + 1);
+			strcpy(copy2, lex);
+			token.lexeme = copy2;
 			token.type = INVALID;
 			return token;
 		}
 	}
-
-	// if we read the entire file and no closing brackets are found,
-	// mark as invalid
-	token.lexeme = lex;
-	token.type = INVALID;
+	if(current_state > __VALID_END_STATES){
+		token.type = NUMBER;
+	}else{
+		token.type = INVALID;
+	}
 	return token;
 }
 
-Token* lexical_analysis(char* fname){
+Token* lexical_analysis(const char* fname){
 	Token* tokens = (Token*)malloc(2048 * sizeof(*tokens));
 
 	// Check for malloc Failure
@@ -164,124 +214,113 @@ Token* lexical_analysis(char* fname){
 	}
 
 	// read character by character
-	char buf[2];
-	char keyval_pairs[1024];
-	while(fgets(buf, sizeof buf, fp) != NULL){
-		char curr = buf[0];
+	int curr;
+	while((curr = fgetc(fp)) != EOF){
 		switch (curr) {
 			case '{':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
+
 				tokens[curr_token_idx].type = OBJ_BEGIN;
-				printf("Found beggining bracket");
+				curr_token_idx++;
+				printf("Found beggining bracket\n");
 				break;
 			case '}':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
 				tokens[curr_token_idx].type = OBJ_END;
-				printf("Found end bracket");
+				curr_token_idx++;
+				printf("Found end bracket\n");
 				break;
 			case '[':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
 				tokens[curr_token_idx].type = ARR_BEGIN;
-				printf("Found beggining array bracket");
+				curr_token_idx++;
+				printf("Found beggining array bracket\n");
 				break;
 			case ']':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
 				tokens[curr_token_idx].type = ARR_END;
-				printf("Found end array bracket");
+				curr_token_idx++;
+				printf("Found end array bracket\n");
 				break;
 			case ':':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
 				tokens[curr_token_idx].type = PUNCTUATOR;
-				printf("Found colon");
+				curr_token_idx++;
+				printf("Found colon\n");
 				break;
 			case ',':
-				tokens[curr_token_idx].lexeme = buf;
+				tokens[curr_token_idx].lexeme = malloc(2);
+
+				tokens[curr_token_idx].lexeme[0] = curr;
+				tokens[curr_token_idx].lexeme[1] = '\0';
 				tokens[curr_token_idx].type = COMMA;
-				printf("Found comma");
+				curr_token_idx++;
+				printf("Found comma\n");
 				break;
 			case '\"':
+				printf("Found double-quote string\n");
 				tokens[curr_token_idx] = get_string_token(fp);
-				printf("Found double-quote string");
+				curr_token_idx++;
 				break;
 			// skip the trailing whitespaces
 			case ' ':
-				printf("Skipping whitespace");
+				printf("Skipping whitespace\n");
+				break;
+			case '\n':
+				printf("Found EOF final char\n");
 				break;
 			case EOF:
-				tokens[curr_token_idx].lexeme = buf;
-				tokens[curr_token_idx].type = FILE_EOF;
-				printf("Found EOF");
+				printf("Found EOF\n");
 				break;
 			default:
-				if(isdigit(curr) || strcmp(&curr, "-")){
-					// TODO: handle cases for numbers and strings
+				char aux[2];
+				aux[0] = curr;
+				aux[1] = '\0';
+				if(isdigit(curr) || strcmp(aux, "-")){
+					printf("checking if valid numeric token\n");
+					tokens[curr_token_idx] = get_numeric_token(fp, curr);
+					curr_token_idx++;
+				} else if(strchr("tfn", curr)){
+					// TODO: Handle checking for true, false or null
+				}else{
+					tokens[curr_token_idx].lexeme = malloc(2);
+
+					tokens[curr_token_idx].lexeme[0] = curr;
+					tokens[curr_token_idx].lexeme[1] = '\0';
+					tokens[curr_token_idx].type = INVALID;
+					curr_token_idx++;
+					printf("Unkown character\n");
 				}
-				printf("Unkown character");
 				break;
 		}
-		curr_token_idx++;
 	}
 	fclose(fp);
-	printf("key-value pairs: %s\n", keyval_pairs);
 	return tokens;
 }
 
 int main(){
 	const char* fname = "./test.json";
-	int is_ok = EXIT_FAILURE;
-
-	FILE* fp = fopen(fname, "r");
-	if (!fp){
-		perror("File opening failed");
-		return is_ok;
+	Token* tokens = lexical_analysis(fname);
+	Token* p = tokens;
+	while(p != NULL && p->lexeme != NULL){
+		printf("Token: %s, Type: %s\n", p->lexeme, stringFromToken(p->type));
+		p++;
 	}
-
-	// read character by character
-	char buf[2];
-	char keyval_pairs[1024];
-	char prev;
-	char* first = fgets(buf, sizeof buf, fp);
-	if(*first == '{'){
-		printf("first character: %s\n", first);
-	}
-	else{
-		perror("File is not a valid JSON");
-		return 1;
-	}
-	while(fgets(buf, sizeof buf, fp) != NULL){
-		if(buf[0] != '\n' && buf[0] != ' ' && buf[0] != '\0'){
-			prev = buf[0];
-			if( prev != '}'){
-				strcat(keyval_pairs, &prev);
-			}
-		}
-	}
-	printf("key-value pairs: %s\n", keyval_pairs);
-	if( prev == '}'){
-		printf("found last character: %s\n", &prev);
-	}
-	else{
-		perror("File is not a valid JSON");
-		return 1;
-	}
-
-	// tokenize the key-value pairs
-	char* token = strtok(keyval_pairs, ",");
-	while(token != NULL){
-		printf("token: %s \n", token);
-		token = strtok(NULL, " , ");
-		char* key = strtok(token, ":");
-		// check if the key is a string
-		if (check_str_delim(key)){
-			key = strtok(NULL, ":");
-			// TODO:
-			// check if value is either string, int, bool or JSON
-		}else{
-			perror("File is not a valid JSON");
-			return 1;
-		}
-	}
-
-	fclose(fp);
-	return is_ok;
+	// free(tokens);
+	return 0;
 }
